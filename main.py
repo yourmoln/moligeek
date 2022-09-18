793af750acea8477392570c3e7273bec0fdb278c
CoolPlayLin <CoolPlayLin@qq.com>
2022-09-18 14:51:10 +0800

    Fix Bug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ script_path = os.path.split(os.path.realpath(__file__))[0]
 sys.path.append(script_path)
 from moligeek import *
 
-<<<<<<< HEAD
+
 ssl = input("是否为https://\n[1]是\n[2]否\n")
 if ssl in ["是", "1"]:
     ssl = "https://"
@@ -23,7 +23,7 @@ except:
     print("Warning: 当前域名检测不到IP可能无法进行进行操作")
 
 mode = input("请选择模式:\n[1]下载源码\n[2]ping\n[3]获取ip\n[4]提交表单\n[5]后台扫描\n[6]洪水攻击\n")
-=======
+
 mainmode = input("请选择模式:\n[1]web\n[2]network\n")
 if mainmode in ["1", "web"]:
     ssl = input("是否为https://\n[1]是\n[2]否\n")
@@ -52,9 +52,6 @@ if mainmode in ["1", "web"]:
 if mainmode in ["2", "network"]:
     target_ip = input("请输入目标ip:")
     mode = input("请选择模式:\n[1]ddos\n[2]ping\n")
->>>>>>> 5458b475586d0d184e6acb0c2026184edd26d6d6
-
-
 
     if mode in ["1", "ddos"]:
         network.ddos_attack(target_ip)
