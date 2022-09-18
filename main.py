2de40a6dc6c2802b6d1a32c7de8318656f9ed2bd
CoolPlayLin <CoolPlayLin@qq.com>
2022-09-18 14:57:11 +0800

    Delete Some Code

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 import sys
 import os
-from socket import gethostbyname
 script_path = os.path.split(os.path.realpath(__file__))[0]
 sys.path.append(script_path)
 from moligeek import *
-
-
-ssl = input("是否为https://\n[1]是\n[2]否\n")
-if ssl in ["是", "1"]:
-    ssl = "https://"
-elif ssl in ["否", "2"]:
-    ssl = "http://"
-else:
-    print("未选择，默认为是")
-    ssl = "https://"
-name = input("请输入链接:"+ssl)
-url = ssl+name
-
-try:
-    print("域名",name,"IP："+gethostbyname(name)+"(仅供参考)")
-except:
-    print("Warning: 当前域名检测不到IP可能无法进行进行操作")
-
-mode = input("请选择模式:\n[1]下载源码\n[2]ping\n[3]获取ip\n[4]提交表单\n[5]后台扫描\n[6]洪水攻击\n")
 
 mainmode = input("请选择模式:\n[1]web\n[2]network\n")
 if mainmode in ["1", "web"]:
