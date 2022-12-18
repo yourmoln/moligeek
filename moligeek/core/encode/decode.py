bc92840ca5fe23e9c25719e1995814ffe23225cd
墨泠 <l1269636755@gmail.com>
2022-12-18 12:55:54 +0800

    优化栅栏密码解密算法

--- moligeek/core/encode/decode.py
+++ moligeek/core/encode/decode.py
@@ -22,28 +22,14 @@ class todecode:
         print("unicode:",self.unicode(text))
 
 class derail:
-    def decode(self,text,lengh):
-        text = list(text)
-        l = len(text)
-        tmp = ['']*l
-        add = l%lengh
-        if add > 0:
-            for i in range(add):
-                text.append("")
-                tmp.append("")
-            print(text)
-        gn = int(len(text)/lengh)
-        for i in range(lengh):
-            for j in range(gn):
-                if i*gn+j < l and i+lengh*j < l:
-                    tmp[i+lengh*j]=text[i*gn+j]
-                elif i+lengh*j > l-1 and i*gn+j < l:
-                    print(text[i*gn+j])
-                    tmp[(i+lengh*j - l - 1)*lengh - 1] = text[i*gn+j]
-        output = ''.join(tmp)
-        return output
+    def decode_rail_fence_cipher(self,cipher: str, num_rails: int) -> str:
+        # 将密文分成 num_rails 个字符一组
+        groups = [cipher[i::num_rails] for i in range(num_rails)]
+        # 将每组的字符串连接起来，形成明文
+        return ''.join([''.join(group) for group in groups])
     def __init__(self,text,lengh):
-        print(self.decode(text,lengh))
+        print(self.decode_rail_fence_cipher(text,lengh))
+
 if __name__ == "__main__":
     #a = todecode("5L2g5aW9")
     a = derail("123456",5)
