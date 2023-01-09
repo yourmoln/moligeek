ab2a74784cb522f95f4f6671e0086fb5bb0e08cd
喵不嗷 <617802189@qq.com>
2023-01-09 15:00:13 +0800

    feat: 添加栅栏密码的加解密模块

--- moligeek/core/encode/decode.py
+++ moligeek/core/encode/decode.py
@@ -33,15 +33,15 @@ class todecode:
         print("URL:",self.url(text))
         print("unicode:",self.unicode(text))
 
-class derail:
-    def decode_rail_fence_cipher(self,cipher: str, num_rails: int) -> str:
-        # 将密文分成 num_rails 个字符一组
-        groups = [cipher[i::num_rails] for i in range(num_rails)]
-        # 将每组的字符串连接起来，形成明文
-        return ''.join([''.join(group) for group in groups])
-    def __init__(self,text,lengh):
-        print(self.decode_rail_fence_cipher(text,lengh))
+# class derail:
+#     def decode_rail_fence_cipher(self,cipher: str, num_rails: int) -> str:
+#         # 将密文分成 num_rails 个字符一组
+#         groups = [cipher[i::num_rails] for i in range(num_rails)]
+#         # 将每组的字符串连接起来，形成明文
+#         return ''.join([''.join(group) for group in groups])
+#     def __init__(self,text,lengh):
+#         print(self.decode_rail_fence_cipher(text,lengh))
 
 if __name__ == "__main__":
     a = todecode("5L2g5aW9")
-    a = derail("123456",5)
+    # a = derail("123456",5)
---
+++ moligeek/core/encode/fence.py
@@ -0,0 +1,43 @@
+import math
+
+def decrypt(cipher, key):
+    """`栅栏密码`的解密函数. 与`千千秀字`的结果一致."""
+    assert type(key) is int
+
+    cipsize = len(cipher)
+    gsize = math.ceil(cipsize / key)
+    psize = gsize * key - cipsize
+    common_count = key - psize        # 正常解码的行数
+
+    res = ''
+    for i in range(gsize):
+        j = i
+        while j < cipsize and (i < common_count or j < common_count * gsize):
+            res += cipher[j]
+            j += gsize if j < common_count * gsize else gsize - 1
+    return res
+
+def encrypt(plain, key):
+    """`栅栏密码`的加密函数. """
+    assert type(key) is int
+
+    plain_size = len(plain)
+    gcount = math.ceil(plain_size / key)
+    common_size = key * (1 - gcount) + plain_size
+
+    res = ''
+    for i in range(key):
+        j = i
+        while j < plain_size and (i < common_size or j < key * (gcount - 1)):
+            res += plain[j]
+            j += key
+    return res
+
+if __name__ == "__main__":
+    plain = "123456789abcdef"
+    key = 4
+    _enc = encrypt(plain, key)
+    _dec = decrypt(_enc, key)
+    print("_enc:", _enc)
+    print("_dec:", _dec)
+    
