c564c2ffeb8a6ff79909b3a235a27037baced86a
miaobuao <miaobuao@outlook.com>
2023-06-24 03:54:07 +0800

    feat: async guess pwd

--- moligeek/core/zip/__init__.py
+++ moligeek/core/zip/__init__.py
@@ -1,12 +1,13 @@
 import zipfile
 import time
-import threading
-from tqdm import tqdm
+import asyncio
+from tqdm.asyncio import tqdm
 from itertools import permutations
 
 PERMUTE = b"1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+-=[{}]\|;:,<.>/?'\" "
 
 def try_open_zip(file, pwd):
+    assert isinstance(file, zipfile.ZipFile)
     try:
         if isinstance(pwd, str):
             pwd = pwd.encode('utf8')
@@ -26,35 +27,57 @@ def zipkey(file):
             print("破解成功，压缩包密码为 %s ,用时 %s s"%(number,int(time.time()-start_time)))
             break
 
-
 def key_generator(start=None, end=None):
     n_size = 0 if start is None else int(start)
     if end is None:
         while True:
             n_size += 1
             for k in permutations(PERMUTE, n_size):
-                yield bytes(k)
+                yield bytes(k), n_size
     else:
         end = int(end)
         while True:
             n_size += 1
             if n_size <= end:
                 for k in permutations(PERMUTE, n_size):
-                    yield bytes(k)
+                    yield bytes(k), n_size
 
 def zipkey_plus(file, min_length=None, max_length=None):
     start_time = time.time()
     zfile = zipfile.ZipFile(file)
-    break_out_flag = False
-    with tqdm(key_generator(min_length, max_length)) as t:
-        for pwd in t:
-            if try_open_zip(zfile, pwd):
-                break_out_flag = True
-                break
-    if break_out_flag:
-        print("破解成功，压缩包密码为 %s ,用时 %s s"%(pwd.decode("utf8"), int(time.time() - start_time)))
+
+    async def block(pwd):
+        if try_open_zip(zfile, pwd):
+            return pwd
+        return False
+
+    async def generator():
+        with tqdm(key_generator(min_length, max_length)) as bar:
+            # queue = []
+            # max_q_size, q_size = 10000, 0
+            # last_size = -1
+            async for pwd, n_size in bar:
+                if await block(pwd):
+                    return pwd
+                # if q_size >= max_q_size:
+                #     res = await asyncio.gather(*queue)
+                #     for p in set(res):
+                #         if p:
+                #             return p
+                #     queue = []
+                #     q_size = 0
+                # queue.append(block(pwd))
+                # q_size += 1
+        return None
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(generator())
+    loop.run_until_complete(future)
+    pwd = future.result()
+    if pwd:
+        print("破解成功，压缩包密码为 %s ,用时 %s s"%(pwd.decode("utf8"), time.time() - start_time))
     else:
         print("破解失败！")
+    return pwd
 
 if __name__ == "__main__":
     # zipkey_plus(r"C:\Users\yourm\Desktop\1\flag.zip")
