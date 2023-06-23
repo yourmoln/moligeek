4d647514f20025b01dd646f101d51a26d4200265
yourmoln <yourmoln@outlook.com>
2023-06-23 23:29:29 +0800

    add:压缩包暴力破解

--- moligeek/core/zip/__init__.py
+++ moligeek/core/zip/__init__.py
@@ -1,22 +1,22 @@
 import zipfile,time,threading,meo
-
+from tqdm import tqdm
 def zipkey(file):
     def zipopen(file,password):
         try:
-            #print(password)
             password = str(password)
-            file.extractall(path='./', pwd=password.encode('utf-8'))
-            print("破解成功，压缩包密码为 %s ,用时 %s"%(password,time.time-start_time))
+            file.extractall(path='./zipout/', pwd=password.encode('utf-8'))
+            return 1
         except:
-            pass
+            return 0
     start_time = time.time()
     zfile = zipfile.ZipFile(file)
-    for number in range(999999999):
-        tzip = threading.Thread(target=zipopen, args=(zfile, number))
-        tzip.start()
-        tzip.join
+    print("开始爆破(纯数字)")
+    for number in tqdm(range(999999999)):
+        if zipopen(zfile,number) == 1:
+            print("破解成功，压缩包密码为 %s ,用时 %s s"%(number,int(time.time()-start_time)))
+            break
 
 if __name__ == "__main__":
-    zipkey(r"C:\Users\Administrator\Desktop\1231.zip")
+    zipkey(r"C:\Users\yourm\Desktop\1\flag.zip")
     ...
 
