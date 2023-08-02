ce9eadc9d1332951375b18b61db6ece7a330c74d
CoolPlayLin <CoolPlayLin@qq.com>
2023-08-02 18:59:56 +0800

    Fix Copyright & License

--- moligeek/__version__.py
+++ moligeek/__version__.py
@@ -4,5 +4,5 @@ __author__   = "yourmoln"
 __author_email__ = "yourmoln@outlook.com"
 __url__     = "https://github.com/yourmoln/moligeek"
 __description__ = "一款逐渐完善的python集成工具,努力为开发者提供最大的便利"
-__license__ = "Apache 2.0"
-__copyright__ = 'Copyright yourmoln'
+__license__ = "Apache-2.0 license"
+__copyright__ = 'Copyright (C) 2023 yourmoln'
