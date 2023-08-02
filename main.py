adc0cde0a398b0e7f8b7079f01d84d7181cd68e8
墨泠moln <yourmoln@outlook.com>
2023-08-02 20:07:48 +0800

    fix:调用库时自动弹出 (#31)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ =='__main__':
     script_path = os.path.split(os.path.realpath(__file__))[0]
     sys.path.append(script_path)
     from moligeek import *
-
+    start = start()
     mainmode = input("请选择模式:\n[0]本机信息\n[1]web\n[2]network\n[3]LAN\n[4]密文处理\n[5]压缩包破解\n")
     if mainmode in ["0", "本机信息"]:
         print("获取信息中...")
@@ -73,7 +73,7 @@ if __name__ =='__main__':
         mode = input("请选择模式:\n[1]泛洪攻击\n[2]洪攻击\n[3]ping\n")
 
         if mode in ["1", "泛洪攻击"]:
-            meo.screen.red_font("注意！此功能极有可能耗尽你的宽带")
+            print("\033[31m注意！此功能极有可能耗尽你的宽带\033[0m")#红色字体
             port = int(input("请输入起始端口:"))
             d = network.Ddos(target_ip, port = port)
             t = threading.Thread(target=d.all, args=())
@@ -81,7 +81,7 @@ if __name__ =='__main__':
             while True:
                 print ("\r已发送 %s 个包到 %s 通过端口:%s"%(d.data['sent'],d.ip,d.data['port']),end="")
         if mode in ["2", "洪攻击"]:
-            meo.screen.red_font("注意！此功能极有可能耗尽你的宽带")
+            print("\033[31m注意！此功能极有可能耗尽你的宽带\033[0m")#红色字体
             port = int(input("请输入目标端口:"))
             d = network.Ddos(target_ip, port = port)
             t = threading.Thread(target=d.one, args=())
